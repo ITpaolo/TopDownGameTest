7e167c313ed96329d63dc0c15c50fabc7b74a2e2
Paolo Chiavino <paolo.chiavino@itspoon.com>
2016-11-15 15:53:56 +0100

    Update

--- js/play.js
+++ js/play.js
@@ -98,9 +98,11 @@ var playState = {
 
         sprite = this.game.add.sprite(50, 500, 'zombies');
         sprite.anchor.setTo(0.5, 0.5);
+        this.game.physics.arcade.enable(sprite);
 
         spriteBot1 = this.game.add.sprite(50, 300, 'zombies');
         spriteBot1.anchor.setTo(0.5, 0.5);
+        this.game.physics.arcade.enable(spriteBot1);
 
         this.game.physics.arcade.enable(sprite, Phaser.Physics.ARCADE);
         this.game.physics.arcade.enable(zombies, Phaser.Physics.ARCADE);
@@ -187,17 +189,10 @@ var playState = {
         this.game.physics.arcade.collide(sprite, stonebigs);
         this.game.physics.arcade.collide(walls, stonebigs);
 
-        this.game.physics.arcade.moveToObject(spriteBot1, sprite, 10, 10);
-        this.game.physics.arcade.enable(sprite);
-        this.game.physics.arcade.enable(spriteBot1);
-
-        sprite.body.velocity.x = 0;
-        sprite.body.velocity.y = 0;
-        sprite.body.angularVelocity = 0;
+        this.game.physics.arcade.moveToObject(spriteBot1, sprite, 150);
 
         spriteBot1.body.velocity.x = 0;
         spriteBot1.body.velocity.y = 0;
-        spriteBot1.body.angularVelocity = 0;
 
         sprite.rotation = game.physics.arcade.angleToPointer(sprite);
 
